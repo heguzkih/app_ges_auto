92853a3fe6650e535ba0d921c7c5c9f557c685c9
Eduardo Hortiguela <eduardohortiguela@gmail.com>
2021-02-15 23:04:51 +0100

    implementacion alumnado

---
+++ app/src/main/java/com/example/ges_auto/updateAlumno.java
@@ -0,0 +1,14 @@
+package com.example.ges_auto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class updateAlumno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_alumno);
+    }
+}
