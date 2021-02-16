dc582933cc13bca3a0beb5f46a33a36e0efc85b0
Eduardo Hortiguela <eduardohortiguela@gmail.com>
2021-02-16 04:42:47 +0100

    crear practicas por profesro

---
+++ app/src/main/java/com/example/ges_auto/CrearPractica.java
@@ -0,0 +1,14 @@
+package com.example.ges_auto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearPractica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_practica);
+    }
+}
