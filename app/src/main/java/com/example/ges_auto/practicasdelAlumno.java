b7eee349e25a66a443cb7174bb56bf05422a7aac
Eduardo Hortiguela <eduardohortiguela@gmail.com>
2021-02-17 11:09:25 +0100

    fin practica alumno

---
+++ app/src/main/java/com/example/ges_auto/practicasdelAlumno.java
@@ -0,0 +1,14 @@
+package com.example.ges_auto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class practicasdelAlumno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practicasdel_alumno);
+    }
+}
